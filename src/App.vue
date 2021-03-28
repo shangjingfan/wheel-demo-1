74c05b685971bbe75e11d2119f8fd4cd8a0f6279
shangjingfan <876625909@qq.com>
2021-03-28 13:36:57 +0800

    更新Switch

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <template>
+  <Button>测试按钮</Button>
+  <hr />
+  <Switch v-model:value="switchVlaue" />
+  <Tabs v-model:selected="x">
+    <Tab title="tab1">导航1</Tab>
+    <Tab title="tab2">导航2</Tab>
+  </Tabs>
+
+  <hr />
+  <Button @click="y = !y">打开对话框</Button>
+  <Dialog v-model:visible="y" :closeOnClickOverlay="false"
+    ><template v-slot:content>
+      <p>你好</p>
+      <p>非常好</p>
+    </template>
+    <template v-slot:title>
+      <strong>标题</strong>
+    </template>
+  </Dialog>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Button, Dialog, Switch, Tabs, Tab } from "wheel-ui-sjf";
+import "wheel-ui-sjf/dist/lib/wheel.css";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Button,
+    Dialog,
+    Switch,
+    Tabs,
+    Tab,
+  },
+  data() {
+    return {
+      switchVlaue: false,
+      x: "tab1",
+      y: false,
+    };
+  },
+};
 </script>
 
 <style>
